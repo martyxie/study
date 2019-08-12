bfb89e8291fa1201997008170bca205a81c83dba
xiexiang <xiexiang@sunniwell.net>
2019-08-12 15:47:55 +0800

    MODIFY:添加继承 & 派生的理解

--- c++/class.cpp
+++ c++/class.cpp
@@ -31,20 +31,44 @@ int box::get_box_soc(void)
 
 void box::set_box_soc(int len,int w,int h)
 {
+	cout << "set data" << endl;
 	length = len;
 	with = w;
 	higth = h;
 	return ;
 }
 
+// 继承(nbox 继承了 box,即 nbox 为box的派生类，基类为 box)
+class nbox:public box
+{
+	public:
+		int get_box_soc(void);
+		int set_nbox_soc(int m);
+	private:
+		int mac;
+};
+
+int nbox::set_nbox_soc(int m)
+{
+	mac = m;
+}
+int nbox::get_box_soc(void)
+{
+	return mac ;
+}
+
 int main(void)
 {
 	box *pbox = new box;
+	nbox newbox;
 	if(pbox)
 	{
 		pbox->set_box_soc(10,4,5);
 		cout << pbox->get_box_soc()<<endl;
 		delete pbox;
 	}
+	newbox.set_box_soc(10,6,9);
+	newbox.set_nbox_soc(1000);
+	cout << newbox.get_box_soc() << endl;
 	return 0;
 }
